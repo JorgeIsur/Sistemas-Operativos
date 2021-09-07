780f1578d52de429c0567b6b101d5539e8915bb5
JorgeIsur <ramirezisur@gmail.com>
2021-09-07 18:30:13 -0500

    Agregado miinodo

---
+++ Semana4/miinodo/fuente/miinodo/miinodo.c
@@ -0,0 +1,32 @@
+#include "miinodo.h"
+
+int miinodo(char *nombre,char *archivo){
+    struct dirent *entradadirectorio;
+    DIR *directorio;
+    if ((directorio=opendir(nombre))==NULL)
+    {
+        fprintf(stderr,"\aNo pude abrir el directorio %s",nombre);
+        perror("\n");
+        return(-1);
+    }
+    while (entradadirectorio=readdir(directorio))
+    {
+        if (entradadirectorio->d_ino!=0)
+        {
+            if (!strncmp(archivo,entradadirectorio->d_name,strlen(archivo)))
+            {
+                return(entradadirectorio->d_ino);
+            }
+            
+        }
+        
+    }
+    if (closedir(directorio)==-1)
+    {
+        fprintf(stderr,"\a No pude cerrar el directorio %s",nombre);
+        perror("\n");
+        return(-1);
+    }
+    return(0);
+    
+}
---
+++ Semana4/miinodo/fuente/miinodo/stub.c
@@ -0,0 +1,21 @@
+#include "miinodo.h"
+
+int main(int argc, char **argv[])
+{
+    ino_t inodo;
+    if (argc!=3)
+    {
+        fprintf(stderr,"Uso:\n");
+        fprintf(stderr,"directorio archivo\n");
+        exit(1);
+    }
+    if ((inodo=miinodo(argv[1],argv[2]))==-1)
+    {
+        fprintf(stderr,"Fallo miinodo\n");
+        exit(2);
+    }
+    else if(inodo) fprintf("El inodo de %s es %i\n",argv[2],inodo);
+    else
+            printf("El archivo %s no se encuentra en el directorio %s\n",argv[2],argv[1]);
+    return 0;
+}

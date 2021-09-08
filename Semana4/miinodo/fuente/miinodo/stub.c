8cd68e929423ad6c0ffa06bcb12f3fe6f67868dc
JorgeIsur <ramirezisur@gmail.com>
2021-09-08 14:53:02 -0500

    fix...

--- Semana4/miinodo/fuente/miinodo/stub.c
+++ Semana4/miinodo/fuente/miinodo/stub.c
@@ -14,7 +14,9 @@ int main(int argc, char **argv[])
         fprintf(stderr,"Fallo miinodo\n");
         exit(2);
     }
-    else if(inodo) fprintf("El inodo de %s es %i\n",argv[2],inodo);
+    if(inodo>0){ 
+        fprintf("El inodo de %s es %i\n",argv[2],inodo);
+    }
     else
             printf("El archivo %s no se encuentra en el directorio %s\n",argv[2],argv[1]);
     return 0;
